665ef6c5ed4ad5090c30d07cd0c15d9716a12413
GuoSiMin <865316371@qq.com>
2016-06-07 00:44:03 +0800

    判断对象是否为空，数组随机一个，call应用

---
+++ test/数组随机一个/test1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/6/7.
+ */
+//                                                     *
+//                                                   ****
+//                                                 *******
+//毕业好久，还想着高考，在此高考前夕，无聊做的练习\(- 0 -)/
+var a = ["清华","北大"];
+console.log("我觉的我还是该上"+a[Math.floor(Math.random()*a.length-1)+1]);
